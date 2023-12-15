149a76fb1862e669a1fe3ba609f1c89d1ba5374a
granth2023 <grantharris38@gmail.com>
2023-12-15 10:37:38 -0500

    pseudo code

---
+++ controllers/controllers.js
@@ -0,0 +1,10 @@
+//what to do here?
+
+// how do we strip out the writes and the reads?
+
+//how do we import the write file from the fs?  
+
+//how do we define our writing toDos for JSOn? 
+//is it a fucnction? What passes through it? How will it take the date we want to write? what variable is tthat? Can we make this an arrow fucntion? 
+//how do we use the middleware? can we pass it through our function? 
+// how do we return error and data?  what are we passing through writeFile? how do we include the path, write dta and the function for error and data? 
