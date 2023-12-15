a963d62ef00d0f30dd7ac7182972e31c23d5b829
granth2023 <grantharris38@gmail.com>
2023-12-15 10:38:38 -0500

    options for controller fucntions

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -8,3 +8,8 @@
 //is it a fucnction? What passes through it? How will it take the date we want to write? what variable is tthat? Can we make this an arrow fucntion? 
 //how do we use the middleware? can we pass it through our function? 
 // how do we return error and data?  what are we passing through writeFile? how do we include the path, write dta and the function for error and data? 
+//should i have function simply only do the write? can it do more? why seperate it? should i add the response logic? how do you seperate concenr of controller logic? 
+
+// what else do we import? 
+//do we also get the request and response in here? do we want the request and resposne in controller? 
+
