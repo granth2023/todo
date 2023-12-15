ca20613d1be7f519f0a4735051cdcef515535935
granth2023 <grantharris38@gmail.com>
2023-12-15 10:06:25 -0500

    put request

--- front-end/index.js
+++ front-end/index.js
@@ -11,3 +11,11 @@ console.log(getToDos())
 //we need to install middleware to handle this 
 //install cors
 //go back to server
+
+//what if you get a 200 but not the array --> do you need to ask for a promsie and then do a dot then? 
+
+//what is being sent from the backend to the front end 
+
+// fetch isnt give me the rouse  but instead a response object 
+
+//how do we crunch this with mongoose? how do we write the delete route? 
